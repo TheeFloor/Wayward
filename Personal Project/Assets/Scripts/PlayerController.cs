c1f188a5fa787984d00b0f16e07e39395fe9fea8
TheeFloor <tiffanynvu27@gmail.com>
2023-02-24 10:07:46 -0800

    Added Boundaries

--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float turnSpeed = 1000.0f;
     private float forwardInput;
     private float horizontalInput;
+    public float xRange = 100f;
+    public float zRange = 100f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,10 +20,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Player Movement
         forwardInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+        //Player Bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+      if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+         if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
     }
 }
